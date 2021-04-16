574e291f82d70615c4fe971ae97024460fbe4d0c
Norbert Schnell <norbert.schnell@hs-furtwangen.de>
2021-04-16 17:26:27 +0200

    added comments

--- server.js
+++ server.js
@@ -5,41 +5,50 @@ const WebSocket = require("ws");
 const port = Number(process.env.PORT) || 8000;
 const server = new WebSocket.Server({ port: port });
 // set of connected sockets
-const clientSockets = new Set();
-const displaySockets = new Set();
-const buffers = [];
+const clientSockets = new Set(); // list of ordinary (photo) clients
+const displaySockets = new Set(); // list of display clients
+// images 
+const images = [];
 let counter = 0;
+// when client connects
 server.on("connection", (socket) => {
+    // add socket to list of ordinary clients
     clientSockets.add(socket);
+    // displatch messages from connected client
     socket.on("message", (message) => {
         if (message === "clear") {
+            // clear (sent by clear.html only)
             counter = 0;
-            buffers.length = 0;
+            images.length = 0;
+            // broadcast "clear" to all 
             for (let socket of displaySockets) {
                 socket.send("clear");
             }
         }
         else if (message === "display") {
-            for (let buffer of buffers) {
-                socket.send(buffer);
+            // new connection 
+            for (let image of images) {
+                socket.send(image);
             }
-            clientSockets.delete(socket);
-            displaySockets.add(socket);
+            // add socket to list of display clients
+            clientSockets.delete(socket); // remove from list of ordinary clients
+            displaySockets.add(socket); // add to list of display clients
             console.log(`connected display ${displaySockets.size}`);
         }
         else {
-            const buffer = message;
-            buffers[counter] = buffer;
+            const image = message;
+            images[counter] = image;
             counter = (counter + 1) % 64;
-            console.log(`reveived image ${buffers.length}`);
+            console.log(`reveived image ${images.length}`);
             for (let socket of displaySockets) {
-                socket.send(buffer);
+                socket.send(image);
             }
         }
     });
+    // closing client
     socket.on("close", () => {
-        clientSockets.delete(socket);
-        displaySockets.delete(socket);
+        clientSockets.delete(socket); // remove from list of ordinary clients
+        displaySockets.delete(socket); // remove from list of display clients
     });
 });
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -5,48 +5,57 @@ const port: number = Number(process.env.PORT) || 8000;
 const server: WebSocket.Server = new WebSocket.Server({ port: port });
 
 // set of connected sockets
-const clientSockets: Set<WebSocket> = new Set();
-const displaySockets: Set<WebSocket> = new Set();
+const clientSockets: Set<WebSocket> = new Set(); // list of ordinary (photo) clients
+const displaySockets: Set<WebSocket> = new Set(); // list of display clients
 
-const buffers: Buffer[] = [];
+// images 
+const images: Buffer[] = [];
 let counter: number = 0;
 
+// when client connects
 server.on("connection", (socket) => {
+  // add socket to list of ordinary clients
   clientSockets.add(socket);
 
+  // displatch messages from connected client
   socket.on("message", (message) => {
     if (message === "clear") {
+      // clear (sent by clear.html only)
       counter = 0;
-      buffers.length = 0;
+      images.length = 0;
 
+      // broadcast "clear" to all 
       for (let socket of displaySockets) {
         socket.send("clear");
       }
     } else if (message === "display") {
-      for (let buffer of buffers) {
-        socket.send(buffer);
+      // new connection 
+      for (let image of images) {
+        socket.send(image);
       }
 
-      clientSockets.delete(socket);
-      displaySockets.add(socket);
+      // add socket to list of display clients
+      clientSockets.delete(socket); // remove from list of ordinary clients
+      displaySockets.add(socket); // add to list of display clients
 
       console.log(`connected display ${displaySockets.size}`);
     } else {
-      const buffer: Buffer = <Buffer>message;
+      const image: Buffer = <Buffer>message;
 
-      buffers[counter] = buffer;
+      images[counter] = image;
       counter = (counter + 1) % 64;
 
-      console.log(`reveived image ${buffers.length}`);
+      console.log(`reveived image ${images.length}`);
 
       for (let socket of displaySockets) {
-        socket.send(buffer);
+        socket.send(image);
       }
     }
   });
 
+  // closing client
   socket.on("close", () => {
-    clientSockets.delete(socket);
-    displaySockets.delete(socket);
+    clientSockets.delete(socket); // remove from list of ordinary clients
+    displaySockets.delete(socket); // remove from list of display clients
   });
 });
